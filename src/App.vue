0585894ad55f90d889a97cf44f6e0c5db22932dd
sakura <447061043@qq.com>
2022-01-20 17:07:05 +0800

    style(login): yolo login style

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
 
 <style lang="less">
